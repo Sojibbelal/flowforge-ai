b8a28dc9490f8b4ee383593f2b9ec4fd0241c119
Sojib Belal <60733261+Sojibbelal@users.noreply.github.com>
2026-04-21 23:20:21 +0600

    Refactor server.js to add root endpoint

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 app.post("/generate", async (req, res) => {
   const { url } = req.body;
 
